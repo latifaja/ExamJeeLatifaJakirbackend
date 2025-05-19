35e4f548fa77ee9eda109794ed369a24d8544ca3
latifaja <#2662019Ma?>
2025-05-19 11:49:14 +0200

    dtos

--- src/main/java/com/example/examlatifajakir/service/CreditService.java
+++ src/main/java/com/example/examlatifajakir/service/CreditService.java
@@ -1,12 +1,20 @@
 package com.example.examlatifajakir.service;
 
-import com.example.examlatifajakir.dto.CreditDTO;
+import com.example.examlatifajakir.dto.*;
 import com.example.examlatifajakir.entities.Credit;
 
 import java.util.List;
 
 public interface CreditService {
+    List<CustomerDto> getAllCustomers();
+    CustomerDto saveCustomer(CustomerDto customerDto);
 
     List<CreditDTO> getAllCredits();
-    CreditDTO createCredit(CreditDTO creditDto);
+    CreditPersonelDto createCreditPersonnel(CreditPersonelDto creditDto);
+    CreditProfDto createCreditProf(CreditProfDto creditDto);
+
+    CreditImmobilierDto createCreditImob(CreditImmobilierDto creditDto);
+
+
+
 }
---
+++ src/main/java/com/example/examlatifajakir/service/CreditServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.examlatifajakir.service;
+
+import com.example.examlatifajakir.dto.*;
+
+import java.util.List;
+
+public class CreditServiceImpl implements CreditService {
+    @Override
+    public List<CustomerDto> getAllCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public CustomerDto saveCustomer(CustomerDto customerDto) {
+        return null;
+    }
+
+    @Override
+    public List<CreditDTO> getAllCredits() {
+        return List.of();
+    }
+
+    @Override
+    public CreditPersonelDto createCreditPersonnel(CreditPersonelDto creditDto) {
+        return null;
+    }
+
+    @Override
+    public CreditProfDto createCreditProf(CreditProfDto creditDto) {
+        return null;
+    }
+
+    @Override
+    public CreditImmobilierDto createCreditImob(CreditImmobilierDto creditDto) {
+        return null;
+    }
+}
