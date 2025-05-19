c5d75046f12eaabe475a465dac2ef6ecb914325f
latifaja <#2662019Ma?>
2025-05-19 09:03:01 +0200

    Initial commit

---
+++ src/main/java/com/example/examlatifajakir/ExamLatifaJakirApplication.java
@@ -0,0 +1,13 @@
+package com.example.examlatifajakir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamLatifaJakirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamLatifaJakirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examlatifajakir/ExamLatifaJakirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examlatifajakir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamLatifaJakirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
